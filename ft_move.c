d4d45df486fc8deb4e71233792c0ae055f84891a
David Zhuravlev <david99111@mial.ru>
2021-11-14 17:55:14 +0300

    ft_swap_works

--- ft_move.c
+++ ft_move.c
@@ -6,7 +6,7 @@
 /*   By: unix <unix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 17:07:36 by unix              #+#    #+#             */
-/*   Updated: 2021/11/14 17:48:02 by unix             ###   ########.fr       */
+/*   Updated: 2021/11/14 17:54:42 by unix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ void	ft_swap(char mode, int *st_a, int *st_b)
 {
 	if (mode == 'a' && ft_slen(st_a) > 1)
 	{
-		ft_swap_val(st_a[ft_slen(st_a) - 1], st_a[ft_slen(st_a) - 2]);
+		ft_swap_val(st_a + ft_slen(st_a) - 1, st_a + ft_slen(st_a) - 2);
 		ft_putstr_fd("sa\n", 1);
 	}
 	else if (mode == 'b' && ft_slen(st_b) > 1)
 	{
-		ft_swap_val(st_a[ft_slen(st_b) - 1], st_a[ft_slen(st_b) - 2]);
+		ft_swap_val(st_b + ft_slen(st_b) - 1, st_b + ft_slen(st_b) - 2);
 		ft_putstr_fd("sb\n", 1);
 	}
 	else
 	{
-		ft_swap_val(st_a[ft_slen(st_a) - 1], st_a[ft_slen(st_a) - 2]);
-		ft_swap_val(st_a[ft_slen(st_b) - 1], st_a[ft_slen(st_b) - 2]);
+		ft_swap_val(st_a + ft_slen(st_a) - 1, st_a + ft_slen(st_a) - 2);
+		ft_swap_val(st_b + ft_slen(st_b) - 1, st_b + ft_slen(st_b) - 2);
 		ft_putstr_fd("ss\n", 1);
 	}
 }
--- ft_sort.c
+++ ft_sort.c
@@ -6,7 +6,7 @@
 /*   By: unix <unix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 20:35:34 by unix              #+#    #+#             */
-/*   Updated: 2021/11/14 17:46:14 by unix             ###   ########.fr       */
+/*   Updated: 2021/11/14 17:54:15 by unix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	ft_sort(int *st_a)
 	st_b = malloc(ft_slen(st_a) * sizeof(int));
 	while (i < ft_slen(st_a) + 1)
 		*(st_b + i++) = -1;
+	ft_swap('b', st_b, st_a);
 	for (int i = 0; st_a[i] != -1; i++)
 	{
 		printf("%d\n", st_a[i]);
