749771e106adb88bde68bff4099be7a992529eb8
David Zhuravlev <david99111@mial.ru>
2021-11-28 14:23:06 +0300

    refactor

--- ft_sort.c
+++ ft_sort.c
@@ -6,11 +6,63 @@
 /*   By: unix <unix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 20:35:34 by unix              #+#    #+#             */
-/*   Updated: 2021/11/25 12:01:03 by unix             ###   ########.fr       */
+/*   Updated: 2021/11/28 14:20:08 by unix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	ft_solve_three(int *st)
+{
+	if (ft_top(st, 3) < ft_top(st, 1) && ft_top(st, 1) < ft_top(st, 2))
+		ft_swap('a', st, st);
+	if (ft_top(st, 1) < ft_top(st, 2) && ft_top(st, 2) < ft_top(st, 3))
+	{
+		ft_swap('a', st, st);
+		ft_rev_rotate('a', st, st);
+	}
+	if (ft_top(st, 1) < ft_top(st, 3) && ft_top(st, 3) < ft_top(st, 2))
+		ft_rotate('a', st, st);
+	if (ft_top(st, 2) < ft_top(st, 3) && ft_top(st, 3) < ft_top(st, 1))
+	{
+		ft_swap('a', st, st);
+		ft_rotate('a', st, st);
+	}
+	if (ft_top(st, 2) < ft_top(st, 1) && ft_top(st, 1) < ft_top(st, 3))
+		ft_rev_rotate('a', st, st);
+}
+
+void	ft_solve_four(int *st_a, int *st_b)
+{
+	if (ft_is_sorted(st_a, 0))
+		return ;
+	if (ft_index(3, st_a) < 2)
+	{
+		while (st_a[3] != 3)
+			ft_rev_rotate('a', st_a, st_b);
+	}
+	while (st_a[3] != 3)
+		ft_rotate('a', st_a, st_b);
+	ft_push('b', st_a, st_b);
+	ft_solve_three(st_a);
+	ft_push('a', st_a, st_b);
+}
+
+void	ft_solve_five(int *st_a, int *st_b)
+{
+	if (ft_is_sorted(st_a, 0))
+		return ;
+	if (ft_index(4, st_a) < 2)
+	{
+		while (st_a[4] != 4)
+			ft_rev_rotate('a', st_a, st_b);
+	}
+	while (st_a[4] != 4)
+		ft_rotate('a', st_a, st_b);
+	ft_push('b', st_a, st_b);
+	ft_solve_four(st_a, st_b);
+	ft_push('a', st_a, st_b);
+}
 
 int	*ft_alloc_b(int size)
 {
@@ -33,7 +85,6 @@ void	ft_sort(int *st_a)
 	if (ft_is_sorted(st_a, 0))
 		return ;
 	st_b = ft_alloc_b(ft_slen(st_a));
-	//print_stacks(st_a, st_b);
 	if (ft_slen(st_a) == 2)
 		ft_rotate('a', st_a, st_b);
 	if (ft_slen(st_a) == 3)
@@ -44,5 +95,4 @@ void	ft_sort(int *st_a)
 		ft_solve_five(st_a, st_b);
 	if (ft_slen(st_a) > 5)
 		ft_solve_common(st_a, st_b);
-	//print_stacks(st_a, st_b);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: unix <unix@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 14:23:40 by unix              #+#    #+#             */
-/*   Updated: 2021/11/28 13:53:27 by unix             ###   ########.fr       */
+/*   Updated: 2021/11/28 14:21:15 by unix             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,42 +22,28 @@ void	print_stacks(int *st_a, int *st_b);
 //RAISE ERROR
 void	ft_raise_error(void);
 
-//VALIDATE
-int		ft_validate_stack(char **st);
-int		ft_str_is_digit(char *str);
-int		ft_has_dups(int *arr, int len);
+//PARSE
+int		*ft_parse_stack(char **st, int len);
 int		ft_is_sorted(int *arr, int direction);
 
 //UTILS
+int		ft_slen(int *stack);
+int		ft_top(int	*st, int pos);
 int		ft_min_arr(int *arr, int len);
 int		ft_max_arr(int *arr, int len);
-int		ft_in(int elem, int min, int max);
-
-//STACK UTILS
-int		ft_slen(int *stack);
 int		ft_index(int val, int *stack);
-void	ft_swap_val(int *d1, int *d2);
-int		*ft_indexate(int *arr, int len);
-int		*ft_parse_stack(char **st, int len);
-void	free_char_arr(char **st);
-int		ft_top(int	*st, int pos);
-void	ft_roll(int id, int *st);
-
-//SORTING FUNCTIONS
-void	ft_sort(int *arr);
 
 //MOVE FUNCTIONS
+void	ft_swap_val(int *d1, int *d2);
 void	ft_swap(char mode, int *st_a, int *st_b);
 void	ft_push(char mode, int *st_a, int *st_b);
 void	ft_rotate(char mode, int *st_a, int *st_b);
 void	ft_rev_rotate(char mode, int *st_a, int *st_b);
 
 //SOLVE
-void	ft_solve_three(int *st);
-void	ft_solve_four(int *st_a, int *st_b);
-void	ft_solve_five(int *st_a, int *st_b);
-void	ft_solve_common(int *st_a, int *st_b);
+void	ft_sort(int *arr);
 int		ft_score(int *st_a, int *st_b);
+void	ft_solve_common(int *st_a, int *st_b);
 
 
 #endif
